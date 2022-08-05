244616d3311b6650d511889d7bae11dba63ff909
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-08-05 19:31:35 +0900

    コンシューマーの実装を整理

--- tutorial-1/src/consumer.rs
+++ tutorial-1/src/consumer.rs
@@ -7,6 +7,10 @@ use lapin::{
 };
 use tracing::info;
 
+mod definitions;
+
+use crate::definitions::{CONNECT_URL, CONSUMER_TAG, QUEUE_NAME};
+
 fn main() {
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", "info");
@@ -14,14 +18,14 @@ fn main() {
 
     tracing_subscriber::fmt::init();
 
-    let address = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
+    let address = std::env::var("AMQP_ADDR").unwrap_or_else(|_| CONNECT_URL.into());
 
     async_global_executor::block_on(async {
         // RabbitMQに接続
         let conn = Connection::connect(&address, ConnectionProperties::default())
             .await
-            .expect("connection error");
-        info!("CONNECTED");
+            .expect("[CONSUMER] connection error");
+        info!("[CONSUMER] connected");
 
         // チャネルを作成
         let channel = conn.create_channel().await.expect("create channel error");
@@ -30,37 +34,38 @@ fn main() {
         // デフォルトエクスチェンジに接続したキューを定義
         let queue = channel
             .queue_declare(
-                "hello",
+                QUEUE_NAME,
                 QueueDeclareOptions::default(),
                 FieldTable::default(),
             )
             .await
-            .expect("declare queue error");
+            .expect("[CONSUMER] declare queue error");
         info!(state=?conn.status().state());
-        info!(?queue, "Declared queue");
+        info!(?queue, "[CONSUMER] declared queue");
 
         // キューにメッセージが到着することを待ち、メッセージを処理するコンシューマーを作成
-        info!("will consume");
+        info!("[CONSUMER] will consume");
         let mut consumer = channel
             .basic_consume(
-                "hello",
-                "consumer",
+                QUEUE_NAME,
+                CONSUMER_TAG,
                 BasicConsumeOptions::default(),
                 FieldTable::default(),
             )
             .await
-            .expect("basic consume error");
+            .expect("[CONSUMER] basic consume error");
         info!(state=?conn.status().state());
 
         // コンシューマーが、キューにメッセージが到着することを待ち、メッセージを処理
         while let Some(delivery) = consumer.next().await {
-            info!(message=?delivery, "received message");
+            info!(message=?delivery, "[CONSUMER] received message");
             // メッセージを正常に処理したら、RabbitMQにメッセージを処理したことを示す肯定応答を返却
             if let Ok(delivery) = delivery {
                 delivery
                     .ack(BasicAckOptions::default())
                     .await
-                    .expect("basic ack error");
+                    .expect("[CONSUMER] basic ack error");
+                info!("[CONSUMER] return ack");
             }
         }
     });
---
+++ tutorial-1/src/definitions.rs
@@ -0,0 +1,3 @@
+pub(crate) const CONNECT_URL: &str = "amqp://127.0.0.1:5672/%2f";
+pub(crate) const QUEUE_NAME: &str = "hello";
+pub(crate) const CONSUMER_TAG: &str = "consumer";
