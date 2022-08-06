3e8fceb91a0dbb985423aa723b7325d4594b6bac
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-08-06 20:15:06 +0900

    ロギング設定を共通化

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,1 +1,10 @@
 pub const RABBIT_MQ_URL: &str = "amqp://127.0.0.1:5672/%2f";
+
+const RUST_LOG_KEY: &str = "RUST_LOG";
+const RUST_LOG_DEFAULT_VALUE: &str = "info";
+
+pub fn set_default_logging_env() {
+    if std::env::var(RUST_LOG_KEY).is_err() {
+        std::env::set_var(RUST_LOG_KEY, RUST_LOG_DEFAULT_VALUE);
+    }
+}
